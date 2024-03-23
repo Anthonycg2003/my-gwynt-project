fdcc7e2bd57532317ed150fbef09a8318121376e
Anthony <55062795+anthonycgarcia2003@users.noreply.github.com>
2024-03-23 14:21:06 -0400

    add UI fuerza

---
+++ Assets/Scripts/Player/Power.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Power : MonoBehaviour
+{
+    public GameObject CC;
+    public GameObject D;
+    public GameObject A;
+    public Text Fuerza_General;
+    public Text Fuerza_CC;
+    public Text Fuerza_D;
+    public Text Fuerza_A;
+    void Start()
+    {
+    }
+    void Update()
+    { 
+        Fuerza_CC.text=Convert.ToString(Get_ChildrenFuerza(CC)); 
+        Fuerza_A.text=Convert.ToString(Get_ChildrenFuerza(A));
+        Fuerza_D.text=Convert.ToString(Get_ChildrenFuerza(D));
+        Fuerza_General.text=Convert.ToString(Get_ChildrenFuerza(CC)+Get_ChildrenFuerza(A)+Get_ChildrenFuerza(D));
+    }
+    public int Get_ChildrenFuerza(GameObject zone)//funcion para acceder a la fuerza de los hijos de cada zona
+    {
+        int fuerza_zone=0;
+        for(int i=0;i<zone.transform.childCount;i++)
+        {
+            int fuerza=zone.transform.GetChild(i).GetComponent<carta>().poder;
+            fuerza_zone+=fuerza;
+        }
+        return fuerza_zone;
+    }
+    
+}
